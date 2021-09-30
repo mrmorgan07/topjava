357877f09301d75435eb188e41cec960a7fa79d4
Maks <mr.korotkov.m@yandex.ru>
2021-09-30 11:21:26 +0300

    Do TODO task cycle method HW0

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -1,13 +1,14 @@
-﻿package ru.javawebinar.topjava.util;
+package ru.javawebinar.topjava.util;
 
 import ru.javawebinar.topjava.model.UserMeal;
 import ru.javawebinar.topjava.model.UserMealWithExcess;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+
 
 public class UserMealsUtil {
     public static void main(String[] args) {
@@ -29,7 +30,54 @@ public class UserMealsUtil {
 
     public static List<UserMealWithExcess> filteredByCycles(List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
         // TODO return filtered list with excess. Implement by cycles
-        return null;
+
+        List<UserMealWithExcess> result = new ArrayList<>();
+        Map<LocalDate, Integer> sumCaloriesMap = new HashMap<>();
+
+        // в sumCaloriesMap пишу сумму калорий по каждому дню,
+        // чтобы при добавлении записи сразу проставлять excess флаг
+        meals.forEach(item -> {
+            if (item.getDateTime().toLocalTime().compareTo(startTime) >= 0
+                    && item.getDateTime().toLocalTime().compareTo(endTime) <= 0) {
+                if (sumCaloriesMap.containsKey(item.getDateTime().toLocalDate())) {
+                    sumCaloriesMap.put(item.getDateTime().toLocalDate(), sumCaloriesMap.get(item.getDateTime().toLocalDate()) + item.getCalories());
+                } else {
+                    sumCaloriesMap.put(item.getDateTime().toLocalDate(), item.getCalories());
+                }
+            }
+        });
+
+        // основной цикл для получения фильтрованного списка
+        meals.forEach(item -> {
+            if (item.getCalories() > caloriesPerDay) {
+
+                //добавляем сразу запись, где идет избыток дневных калорий
+                result.add(new UserMealWithExcess(LocalDateTime.of(item.getDateTime().toLocalDate(),item.getDateTime().toLocalTime()),
+                        item.getDescription(),
+                        item.getCalories(),
+                        true));
+            } else {
+
+                //фильтруем записи по временному промежутку
+                if (item.getDateTime().toLocalTime().compareTo(startTime) >= 0
+                        && item.getDateTime().toLocalTime().compareTo(endTime) <= 0) {
+
+                    // проверяем суммарное за день потребление калорий
+                    if (sumCaloriesMap.get(item.getDateTime().toLocalDate()) > caloriesPerDay) {
+                        result.add(new UserMealWithExcess(LocalDateTime.of(item.getDateTime().toLocalDate(),item.getDateTime().toLocalTime()),
+                                item.getDescription(),
+                                item.getCalories(),
+                                true));
+                    } else {
+                        result.add(new UserMealWithExcess(LocalDateTime.of(item.getDateTime().toLocalDate(),item.getDateTime().toLocalTime()),
+                                item.getDescription(),
+                                item.getCalories(),
+                                false));
+                    }
+                }
+            }
+        }); 
+        return result;
     }
 
     public static List<UserMealWithExcess> filteredByStreams(List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
