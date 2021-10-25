32279f501f72259713c2e1b54406a344b6516978
mrmorgan07 <ms.korotkov.m@yandex.ru>
2021-10-25 18:39:17 +0300

    Revert "Apply 3_07_HW2_optional_select_user.patch"
    This reverts commit ca4e6cf7884d903df1d0b665bc36b01097fa73ca.

--- src/main/java/ru/javawebinar/topjava/web/SecurityUtil.java
+++ src/main/java/ru/javawebinar/topjava/web/SecurityUtil.java
@@ -4,14 +4,8 @@ import static ru.javawebinar.topjava.util.MealsUtil.DEFAULT_CALORIES_PER_DAY;
 
 public class SecurityUtil {
 
-    private static int id = 1;
-
     public static int authUserId() {
-        return id;
-    }
-
-    public static void setAuthUserId(int id) {
-        SecurityUtil.id = id;
+        return 1;
     }
 
     public static int authUserCaloriesPerDay() {
